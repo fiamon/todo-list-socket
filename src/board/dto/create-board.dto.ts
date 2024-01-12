ced5d824e23f68942e77605bfdc3b271ac3552ae
fiamon <juliogustavofi@gmail.com>
2024-01-12 00:18:08 -0300

    fix: create board dto validations

--- src/board/dto/create-board.dto.ts
+++ src/board/dto/create-board.dto.ts
@@ -1,11 +1,13 @@
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 import { User } from 'src/user/entities/user.entity';
 
 export class CreateBoardDto {
   @IsString()
+  @IsNotEmpty()
   title: string;
 
   @IsString()
+  @IsNotEmpty()
   description: string;
 
   users: User[];
